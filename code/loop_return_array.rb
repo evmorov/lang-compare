7664a8e0d28407e6f430426684b2aad9d11eef94
Evgeny Morozov <evmorov@gmail.com>
2015-09-19 09:39:26 +0300

    improve loop return for ruby

--- code/loop_return_array.rb
+++ code/loop_return_array.rb
@@ -1,6 +1,5 @@
-greetings = []
-3.times do |time|
-  greetings.push "#{time + 1}. Hello!"
+greetings = 3.times.map do |time|
+  "#{time + 1}. Hello!"
 end
 p greetings
 # ["1. Hello!", "2. Hello!", "3. Hello!"]
