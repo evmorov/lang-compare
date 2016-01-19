8dcb537f0058195f17eac5e91227f95033520b18
Evgeny Morozov <evmorov@gmail.com>
2016-01-19 23:27:15 +0300

    Add Java examples

---
+++ code/StringCompare.java
@@ -0,0 +1,8 @@
+class StringCompare {
+  public static void main(String[] args) {
+    System.out.println("string" == "string");
+    // true
+    System.out.println("string" != "string");
+    // false
+  }
+}
