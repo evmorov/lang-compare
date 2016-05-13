e1dbd39fb50304d2e5e6c05392368ab68f0447f1
Evgeny Morozov <evmorov@gmail.com>
2016-05-14 00:46:17 +0300

    Hide section if both of langs don't have an example

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -5,11 +5,10 @@ module CustomHelpers
     'java' => 'java'
   }
 
-  def code_file(filename)
-    return 'Not available' unless filename
+  def code_from_file(filename)
     file_extension = CODE_EXTENSION[(File.extname filename).delete('.')]
     code(file_extension) do
-      File.read File.join(Dir.pwd, 'code', filename) rescue return 'No easy way to do that'
+      File.read File.join(Dir.pwd, 'code', filename)
     end
   end
 
