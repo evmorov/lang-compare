b0f005f17833cecd4f3c43d24a314d2b926d1009
Evgeny Morozov <evmorov@gmail.com>
2016-05-19 00:55:16 +0300

    Hide a section if there are no examples

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -17,9 +17,16 @@ module CustomHelpers
     str.tr(' /,', '_').tr('()?', '').downcase
   end
 
-  def one_file_exist?(filename1, filename2)
+  def section_empty?(section, lang1, lang2)
+    section.last.each do |_header, filenames|
+      return false unless no_files?(filenames[lang1.downcase], filenames[lang2.downcase])
+    end
+    true
+  end
+
+  def no_files?(filename1, filename2)
     file1 = filename1 && (File.exist? File.join(Dir.pwd, 'code', filename1))
     file2 = filename2 && (File.exist? File.join(Dir.pwd, 'code', filename2))
-    file1 || file2
+    !(file1 || file2)
   end
 end
