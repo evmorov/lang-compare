062e1e2373d47f17f67167eb56687b29755671fa
Evgeny Morozov <evmorov@gmail.com>
2016-05-25 00:08:38 +0300

    Add one java example

---
+++ code/TypeNullTrueFalse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class TypeNullTrueFalse {
+  public static void main(String[] args) {
+    String notEmpty = "Not empty";
+    System.out.println(notEmpty.isEmpty());
+
+    String shouldBeNull = null;
+    System.out.println(shouldBeNull == null);
+
+    List<String> arr = new ArrayList<String>();
+    System.out.println(arr.isEmpty());
+  }
+}
